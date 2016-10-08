d5d238527ff42ff70bfdb152f3f3e713deeb922a
Vít Novotný <witiko@mail.muni.cz>
2016-10-08 11:56:27 +0200

    Replaced the monochrome labels with the correct version.

--- label/mu/fetch.sh
+++ label/mu/fetch.sh
@@ -6,8 +6,8 @@ shopt -s nullglob
 ../../fetch-pdf.sh \
  's#Univerzita–obecne#Ostatni-pracoviste/base#;
   s#Univerzita-rektorat#Ostatni-pracoviste/rect#;
-  s#.*/(.*)/.*/.*/[^_]*_stitek_outline_cz\.pdf$#filetter-$1-czech.pdf#;
+  s#.*/(.*)/.*/.*/[^_]*_stitek_outline-prostor_cz\.pdf$#filetter-$1-czech.pdf#;
   s#.*/(.*)/.*/.*/[^_]*_stitek_cz\.pdf$#filetter-$1-czech-color.pdf#;
-  s#.*/(.*)/.*/.*/[^_]*_stitek_outline_en\.pdf$#filetter-$1-english.pdf#;
+  s#.*/(.*)/.*/.*/[^_]*_stitek_outline-prostor_en\.pdf$#filetter-$1-english.pdf#;
   s#.*/(.*)/.*/.*/[^_]*_stitek_en\.pdf$#filetter-$1-english-color.pdf#;' \
-  /media/*/gdrive/MU-JVS-AKTUALIZACE2015/**/stitky/pdf-cmyk-*/*_stitek{,_outline}_{cz,en}.pdf
+  /media/*/gdrive/MU-JVS-AKTUALIZACE2015/**/stitky/pdf-cmyk-*/*_stitek{,_outline-prostor}_{cz,en}.pdf
