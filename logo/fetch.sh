81f518667649c53aac31a02cf3e245705aa94dac
Vít Novotný <witiko@mail.muni.cz>
2016-10-09 22:32:17 +0200

    Finished most of the work.

--- logo/fetch.sh
+++ logo/fetch.sh
@@ -4,10 +4,10 @@ set -e
 shopt -s globstar
 shopt -s nullglob
 ../../fetch-pdf.sh \
- 's#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cb\.pdf$#filetter-$1-english.pdf#;
-  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cmyk(-modra)?\.pdf$#filetter-$1-english-color.pdf#;
-  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cb_cz\.pdf$#filetter-$1-czech.pdf#;
-  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cmyk_cz\.pdf$#filetter-$1-czech-color.pdf#;
-  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cb_en\.pdf$#filetter-$1-english.pdf#;
-  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cmyk_en\.pdf$#filetter-$1-english-color.pdf#;' \
+ 's#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cb\.pdf$#muletter-$1-english.pdf#;
+  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cmyk(-modra)?\.pdf$#muletter-$1-english-color.pdf#;
+  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cb_cz\.pdf$#muletter-$1-czech.pdf#;
+  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cmyk_cz\.pdf$#muletter-$1-czech-color.pdf#;
+  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cb_en\.pdf$#muletter-$1-english.pdf#;
+  s#.*/(.*)/.*/[^_]*_(logo|znak|pecet)-cmyk_en\.pdf$#muletter-$1-english-color.pdf#;' \
   /media/*/gdrive/MU-JVS-AKTUALIZACE2015/*/*/{znaky/*_znak-{cb,cmyk,cmyk-modra}{,_{cz,en}},loga/*_{logo,pecet}-{cb,cmyk}{,_{cz,en}}}.pdf 
